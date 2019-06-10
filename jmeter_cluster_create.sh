2c5ab6bdbe9236c4e3108dbad9f7540ecf8fb749
hankwumomo <38854627+hankwumomo@users.noreply.github.com>
2019-06-10 22:27:37 +0800

    Update jmeter_cluster_create.sh

--- jmeter_cluster_create.sh
+++ jmeter_cluster_create.sh
@@ -17,6 +17,9 @@ kubectl version --short
 
 echo "Current list of namespaces on the kubernetes cluster:"
 
+gcloud beta container --project "momo-search" clusters create "standard-cluster-1" --zone "asia-east1-b" --no-enable-basic-auth --cluster-version "1.12.8-gke.6" --machine-type "n1-standard-1" --image-type "COS" --disk-type "pd-standard" --disk-size "100" --metadata disable-legacy-endpoints=true --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" --num-nodes "1" --enable-cloud-logging --enable-cloud-monitoring --enable-ip-alias --network "projects/momo-search/global/networks/mo-gcp" --subnetwork "projects/momo-search/regions/asia-east1/subnetworks/momo-gcp-51" --default-max-pods-per-node "110" --addons HorizontalPodAutoscaling,HttpLoadBalancing --no-enable-autoupgrade --no-enable-autorepair
+
+
 tenant=momotest
 
 
