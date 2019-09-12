71857802cbcb153e01648a52619a0dba9e794ebf
hankwumomo <hankwmomo@gmail.com>
2019-09-12 11:09:22 +0800

    nat update

--- jmeter_deployment.sh
+++ jmeter_deployment.sh
@@ -20,11 +20,10 @@ kubectl version --short
 echo "Current list of namespaces on the kubernetes cluster:"
 
 
-gcloud beta container --project "momo-develop" clusters create "standard-cluster-1" --zone "asia-east1-a" --no-enable-basic-auth --cluster-version "1.13.7-gke.8" --machine-type "n1-standard-1" --image-type "COS" --disk-type "pd-standard" --disk-size "100" --metadata disable-legacy-endpoints=true --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" --num-nodes "3" --no-enable-cloud-logging --no-enable-cloud-monitoring --enable-private-nodes --master-ipv4-cidr "172.16.0.0/28" --enable-ip-alias --network "projects/momo-develop/global/networks/gke-network" --subnetwork "projects/momo-develop/regions/asia-east1/subnetworks/subnet-192" --default-max-pods-per-node "110" --enable-master-authorized-networks --master-authorized-networks 0.0.0.0/0 --addons HorizontalPodAutoscaling,HttpLoadBalancing --enable-autoupgrade --enable-autorepair
-
 
 tenant=momotest
 
+gcloud container clusters get-credentials standard-cluster-1 --zone asia-east1-a --project momo-develop
 
 
 
--- start_test.sh
+++ start_test.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash 
 #Script created to launch Jmeter tests directly from the current terminal without accessing the jmeter master pod.
 #It requires that you supply the path to the jmx file
 #After execution, test script jmx file may be deleted from the pod itself but not locally.
-
+set -x
 working_dir="`pwd`"
 
 #Get namesapce variable
